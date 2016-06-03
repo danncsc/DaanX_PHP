daccfaed586bc478ecdf3c5e7a5ff2719bcf40aa
Lian0123 <lienyungli123@gmail.com>
2016-06-03 08:36:52 +0800

    穩定性提昇

--- get_web_last.php
+++ get_web_last.php
@@ -3,6 +3,10 @@
 本程式已完成
 include的程式
 ==========================*/
+
+    $hear0=get_headers("http://ta.taivs.tp.edu.tw/news/news.asp?PageNo=1&board=1&KEY=0");//空值情況整網頁初始大小
+    $text0=implode("", array_intersect(str_split($hear0[5]), array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));//空值時網站標頭大小（$hear[5]） 並取得其大小（KB）
+
 	$sum=0;//設定記數初始值
 	$read_data=0;//設定記數初始值
 	$read_SQL=mysql_query("SELECT * FROM stu_sgin");//尋找資料表
@@ -14,12 +18,13 @@ include的程式
         for ($i=$number; $i <$number+20 ; $i++) { //將＄i設為上次搜索到的最終資料 並以每二十個資料為一循環
 			$hear=get_headers("http://ta.taivs.tp.edu.tw/news/news.asp?PageNo=1&board=1&KEY=$i");//取得網站標頭（＄i為上次搜尋到的數字累加）
 			echo "在查詢"."$i"."筆資料.\n";
-            $text=implode("", array_intersect(str_split($hear[5]), array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));//選取網站標頭的大小（$hear[5]） 並取得其大小（KB）
-			$read_text=$number+20-$i-1;//當迴圈執行完 但數字未跑完時 迴圈增值的判定
-			if($text>173000&&$read_text>0){//當其大小 大於173000KB 且迴圈未跑完時的判定
+            $text1=implode("", array_intersect(str_split($hear[5]), array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));//選取網站標頭的大小（$hear[5]） 並取得其大小（KB）
+			$text=$text1-$text0;//減去多餘的東西
+            $read_text=$number+20-$i-1;//當迴圈執行完 但數字未跑完時 迴圈增值的判定
+			if($text>400&&$read_text>0){//當其大小 大於400KB 且迴圈未跑完時的判定
 				$array_unmber[]=$i;//將（存在的）資料 存入$array_unmber的陣列中
 			}
-			elseif($text>173000&&$read_text==0){//當其大小 大於173000KB 且迴圈已跑完時的判定
+			elseif($text>400&&$read_text==0){//當其大小 大於400KB 且迴圈已跑完時的判定
 				$array_unmber[]=$i;//將（存在的）資料 存入$array_unmber的陣列中
 				$number=$number+20;//迴圈增值
 			}
